87587b7a56e85c1a70fe926543ec83b2887d0cf5
Wenpei Shao <wenpeishao@gmail.com>
2023-12-03 21:38:07 -0600

    update new plot

--- block_size/run_fio_tests.sh
+++ block_size/run_fio_tests.sh
@@ -22,6 +22,9 @@ for BS in "${BLOCK_SIZES[@]}"; do
     # Reset the SSD after each block size test
     reset_ssd
     LOG_PREFIX="test_bs_${BS}_bw"
+    # Fill the SSD to near capacity before testing
+    #dd if=/dev/zero of=${SSD_DEVICE} bs=1G count=1750G status=progress
+
     echo "Running fio test for block size: $BS KB"
 
     # # Run fio with a log file to capture throughput at different intervals
@@ -33,8 +36,8 @@ for BS in "${BLOCK_SIZES[@]}"; do
     #     --runtime=3000 --ioengine=io_uring --direct=1 --rw=write --bs=${BS}k \
     #     --iodepth=32 --write_bw_log=$LOG_PREFIX --log_avg_msec=100
 
-    fio --name=garbage_collection_test --filename=$SSD_DEVICE --size=10G --ioengine=io_uring \
-    --rw=randwrite --bs=4k --iodepth=32 --direct=1 --runtime=3600 --group_reporting \
+    fio --name=garbage_collection_test --filename=$SSD_DEVICE --ioengine=io_uring \
+    --rw=randwrite --bs=${BS}k --iodepth=1 --direct=1 --runtime=900 --group_reporting \
     --write_bw_log=$LOG_PREFIX --log_avg_msec=100 --time_based
 
 
@@ -44,7 +47,7 @@ for BS in "${BLOCK_SIZES[@]}"; do
 
         if [ -f "$LOG_FILE" ]; then
             awk -v bs=$BS '{print bs","$2/1024","$1/1024}' $LOG_FILE >> $OUTPUT_FILE
-            #rm $LOG_FILE
+            rm $LOG_FILE
         else
             echo "Log file $LOG_FILE not created or not accessible. Skipping..."
         fi
