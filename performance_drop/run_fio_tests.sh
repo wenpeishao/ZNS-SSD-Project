7bb980488dec240b5e20779a93981ecdc2ef5603
Wenpei Shao <clevertonyshao@gmail.com>
2023-12-05 01:26:25 -0600

    update scrip

--- performance_drop/run_fio_tests.sh
+++ performance_drop/run_fio_tests.sh
@@ -10,6 +10,7 @@ BLOCK_SIZES=(4096)
 reset_ssd() {
     echo "Performing a fast reset of the SSD: $SSD_DEVICE"
     sudo blkdiscard $SSD_DEVICE
+    sleep 600  # Allow the SSD some time to stabilize after reset
 }
 
 run_test() {
@@ -20,13 +21,12 @@ run_test() {
 
     for BS in "${BLOCK_SIZES[@]}"; do
         reset_ssd
-        sleep 600
         LOG_PREFIX="test_${RW_TYPE}_bs_${BS}_bw"
 
-        fio --name=garbage_collection_test --filename=$SSD_DEVICE --ioengine=io_uring \
-            --rw=$RW_TYPE --bs=${BS}k --iodepth=32 --direct=1 --runtime=7200 --group_reporting \
+        fio --name=test_${RW_TYPE}_bs_${BS} --filename=$SSD_DEVICE --ioengine=io_uring \
+            --rw=$RW_TYPE --rwmixread=50 --bs=${BS}k --iodepth=64 --direct=1 --runtime=10 --group_reporting \
             --write_bw_log=$LOG_PREFIX --log_avg_msec=500 --time_based
-
+            
         for LOG_FILE in ${LOG_PREFIX}*.log; do
             chmod 777 $LOG_FILE
 
@@ -40,10 +40,15 @@ run_test() {
     done
 }
 
-# Run tests for random and sequential writes
-# run_test "randwrite"
+# Run tests for random write, random read, sequential write, and sequential read
+run_test "randwrite"
+run_test "randread"
 run_test "write"
+run_test "read"
+run_test "randrw"
 
 # Plot results for each test type
 python3 plot_fio_results.py fio_results_randwrite.csv
+python3 plot_fio_results.py fio_results_randread.csv
 python3 plot_fio_results.py fio_results_write.csv
+python3 plot_fio_results.py fio_results_read.csv
